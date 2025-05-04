a59d247468f8869e295ea4c290866a2693bf87c9
Leonardo Castro <twelve.programming@gmail.com>
2025-05-04 04:27:39 -0400

    Adicionei o modo escuro usando o principalmente o JavaScript e o CSS

--- Atividades/2-atividade-07-05/script.js
+++ Atividades/2-atividade-07-05/script.js
@@ -1,3 +1,20 @@
+//Botão Modo Light e Modo Dark
+let trilho = document.getElementById('trilho')
+let body = document.querySelector('body')
+let nav = document.querySelector('nav')
+let footer = document.querySelector('footer')
+let button = document.querySelector('button')
+
+trilho.addEventListener('click', function() {
+    trilho.classList.toggle('dark')
+    body.classList.toggle('dark')
+    nav.classList.toggle('dark')
+    footer.classList.toggle('dark')
+    button.classList.toggle('dark')
+});
+
+
+//Botão Fale Comigo
 document.getElementById('faleComigo').addEventListener('click', function() {
     const meuEmail = "student.leonardocastro@gmail.com";
     alert(`Meu email para contato é: ${meuEmail}`);

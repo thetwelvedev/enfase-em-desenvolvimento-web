7737d154cf52d6144967b244d23afe8f5038380f
Leonardo Castro <twelve.programming@gmail.com>
2025-05-04 02:52:07 -0400

    Adicionando o botão fale comigo

---
+++ Atividades/2-atividade-07-05/script.js
@@ -0,0 +1,5 @@
+document.getElementById('faleComigo').addEventListener('click', function() {
+    const meuEmail = "student.leonardocastro@gmail.com";
+    alert(`Meu email para contato é: ${meuEmail}`);
+});
+console.log("Script está funcionando!");
